a47258c4e7a9c7639dea100ee2466086af85dbb2
CCMitracos <32472605+CCMitracos@users.noreply.github.com>
2017-10-03 19:19:55 -0700

    Update dpc104e.py

--- dpc104e.py
+++ dpc104e.py
@@ -1,2 +1,15 @@
 # DPC 104 [E]
 # Powerplant Simulation
+
+
+play = True
+while (play == True):
+
+  k = int(raw_input("\nNumber of days comissioned >> "))
+  
+  r = 2*(k/3)+(k%3)-(k/100-k/300) - (2*((k/14)/3) + ((k/14)%3)) + k/700
+  
+  print "\nNumber of days to run:", r
+  
+  if (raw_input("\nq to quit")=="q"):
+    play = False
